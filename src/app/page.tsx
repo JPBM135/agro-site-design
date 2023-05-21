e437a5ff469582e22fd379685b7d8cf28d71208a
João Pedro Borges Martins <jpedrobm0@gmail.com>
2023-05-21 19:12:00 -0300

    feat(root): add general concepts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import { CaretCircleDoubleDown } from '@phosphor-icons/react';
 import Image from 'next/image';
 import { useEffect } from 'react';
 import { ImageCarousel } from '@/components/ImageCarousel';
+import { ScrollFadeOutCaret } from '@/components/ScrollFadeOutCaret';
 
 export default function Home() {
 	return (
@@ -18,6 +20,17 @@ export default function Home() {
 					</h2>
 				</div>
 			</div>
+			<ScrollFadeOutCaret />
+			<article className="flex flex-col items-center w-full max-w-7xl gap-10 mt-10 max-lg:mx-10">
+				<h2 className="text-4xl max-lg:text-2xl items-center font-semibold text-center border-b mb-2 pb-2 w-fit text-raisin-black">
+					O que é o plantio?
+				</h2>
+				<p className="text-lg text-justify text-gray-700 mx-20 max-lg:mx-10">
+					Plantio é a definição de plantar sementes na terra, para que ela se desenvolva, e atinja o nível desejado do
+					agricultor. Entretanto, este processo não é somente feito com sementes, pois algumas plantas são propagadas
+					vegetativamente, ou seja, um clone da planta mãe, esse processo é chamado de propagação assexuada.
+				</p>
+			</article>
 			<h2 className="text-4xl max-lg:text-2xl font-semibold text-center border-b mb-2 pb-2 w-fit text-raisin-black">
 				Quais são os métodos de plantio mais utilizados?
 			</h2>
@@ -71,6 +84,24 @@ export default function Home() {
 					</h4>
 				</div>
 			</div>
+			<article className="flex flex-col items-center w-full max-w-7xl gap-10 mt-10 max-lg:mx-10">
+				<h2 className="text-4xl max-lg:text-2xl items-center font-semibold text-center border-b mb-2 pb-2 w-fit text-raisin-black">
+					Sobre a agricultura familiar:
+				</h2>
+				<p className="text-lg text-justify text-gray-700 mx-20 max-lg:mx-10">
+					No país, há diversos tipos de culturas agrícola, desde os modernos aos mais tradicionais, e todos com o mesmo
+					objetivo de produzir alimentos. A agricultura familiar é um sistema em que pessoas da mesma família trabalham
+					em conjunto na terra onde produzem as atividades comerciais.
+				</p>
+				<p className="text-lg text-justify text-gray-700 mx-20 max-lg:mx-10">
+					Além disso, fontes como o “Instituto Brasileiro de Geografia e Estatística (IBGE)”, “Companhia Nacional de
+					Abastecimento (Conab)”, Ministério da Agricultura, “Pecuária e Abastecimento (Mapa)”, “Organização das Nações
+					Unidas (ONU)”, e outros, a Agricultura Familiar é responsável por produzir mais de 80% dos alimentos do mundo.
+					Já no Brasil, foi indicado por pesquisas que a mesma também é responsável por 90% da base econômica dos
+					municípios brasileiros, com produções e comercialização diversificadas de frutas, vegetais, legumes, proteínas
+					e grãos.
+				</p>
+			</article>
 		</main>
 	);
 }
