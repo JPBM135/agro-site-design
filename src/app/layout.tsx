656c15a8d90cafe691d00847db0394a098e95050
João Pedro Borges Martins <jpedrobm0@gmail.com>
2023-05-20 22:34:58 -0300

    chore(meta): move from layout to Head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,46 +1,14 @@
 import { Inter } from 'next/font/google';
+import { Head } from '@/components/Head';
 import { Header } from '@/components/Header';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
-	title: 'CultivaCiência - Sua melhor fonte de conhecimento',
-	description:
-		'CultivaCiência é um site de divulgação científica dedicado ao campo da agricultura. Nosso objetivo é fornecer informações valiosas e atualizadas sobre práticas agrícolas, inovações tecnológicas, tendências de mercado e pesquisa científica relacionada à agricultura. Com uma equipe dedicada de alunos da UNIP apaixonados por ciência e agricultura, buscamos compartilhar conhecimentos e promover a disseminação de boas práticas para melhorar a produtividade, sustentabilidade e eficiência no setor agrícola. Explore nosso conteúdo e descubra as últimas descobertas e avanços na ciência agrícola.',
-	keywords:
-		'agricultura, divulgação científica, UNIP, práticas agrícolas, inovações tecnológicas, tendências de mercado, pesquisa científica, produtividade, sustentabilidade, eficiência',
-	image: 'https://agro-site-design.vercel.app/metadata-image.png',
-	url: 'https://agro-site-design.vercel.app/',
-	'twitter:card': 'summary_large_image',
-	'twitter:site': '@cultivaciencia',
-	'twitter:creator': '@cultivaciencia',
-	'twitter:title': 'CultivaCiência - Sua melhor fonte de conhecimento',
-	'twitter:description':
-		'CultivaCiência é um site de divulgação científica dedicado ao campo da agricultura. Nosso objetivo é fornecer informações valiosas e atualizadas sobre práticas agrícolas, inovações tecnológicas, tendências de mercado e pesquisa científica relacionada à agricultura. Com uma equipe dedicada de alunos da UNIP apaixonados por ciência e agricultura, buscamos compartilhar conhecimentos e promover a disseminação de boas práticas para melhorar a produtividade, sustentabilidade e eficiência no setor agrícola. Explore nosso conteúdo e descubra as últimas descobertas e avanços na ciência agrícola.',
-	'twitter:image': 'https://agro-site-design.vercel.app/metadata-image.png',
-	'twitter:image:alt': 'CultivaCiência - Sua melhor fonte de conhecimento',
-	'twitter:url': 'https://agro-site-design.vercel.app/',
-	'og:title': 'CultivaCiência - Sua melhor fonte de conhecimento',
-	'og:description':
-		'CultivaCiência é um site de divulgação científica dedicado ao campo da agricultura. Nosso objetivo é fornecer informações valiosas e atualizadas sobre práticas agrícolas, inovações tecnológicas, tendências de mercado e pesquisa científica relacionada à agricultura. Com uma equipe dedicada de alunos da UNIP apaixonados por ciência e agricultura, buscamos compartilhar conhecimentos e promover a disseminação de boas práticas para melhorar a produtividade, sustentabilidade e eficiência no setor agrícola. Explore nosso conteúdo e descubra as últimas descobertas e avanços na ciência agrícola.',
-	'og:image': 'https://agro-site-design.vercel.app/metadata-image.png',
-	'og:image:alt': 'CultivaCiência - Sua melhor fonte de conhecimento',
-	'og:url': 'https://agro-site-design.vercel.app/',
-	'og:site_name': 'CultivaCiência',
-	'og:type': 'website',
-	'og:locale': 'pt_BR',
-	'article:author': 'https://jpbm.dev/',
-	'article:publisher': 'ttps://jpbm.dev/',
-	'article:section': 'Agricultura',
-	'article:tag':
-		'agricultura, divulgação científica, UNIP, práticas agrícolas, inovações tecnológicas, tendências de mercado, pesquisa científica, produtividade, sustentabilidade, eficiência',
-	'article:published_time': '2023-05-21T00:35:24.093Z',
-};
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="pt-br">
+			<Head />
 			<body className={`${inter.className} body-background bg-gradient-to-br from-snow to-slate-gray`}>
 				<Header />
 				{children}
---
+++ src/components/Head.tsx
@@ -0,0 +1,53 @@
+export function Head() {
+	return (
+		<head>
+			<meta charSet="utf8" />
+			<meta content="width=device-width, initial-scale=1.0" name="viewport" />
+
+			<title>CultivaCiência - Sua melhor fonte de conhecimento</title>
+			<meta
+				content="CultivaCiência é um site de divulgação científica dedicado ao campo da agricultura. Nosso objetivo é fornecer informações valiosas e atualizadas sobre práticas agrícolas, inovações tecnológicas, tendências de mercado e pesquisa científica relacionada à agricultura. Com uma equipe dedicada de alunos da UNIP apaixonados por ciência e agricultura, buscamos compartilhar conhecimentos e promover a disseminação de boas práticas para melhorar a produtividade, sustentabilidade e eficiência no setor agrícola. Explore nosso conteúdo e descubra as últimas descobertas e avanços na ciência agrícola."
+				name="description"
+			/>
+			<meta
+				content="agricultura, divulgação científica, UNIP, práticas agrícolas, inovações tecnológicas, tendências de mercado, pesquisa científica, produtividade, sustentabilidade, eficiência"
+				name="keywords"
+			/>
+
+			<meta content="website" property="og:type" />
+			<meta content="https://agro-site-design.vercel.app/" property="og:url" />
+			<meta content="CultivaCiência - Sua melhor fonte de conhecimento" property="og:title" />
+			<meta
+				content="CultivaCiência é um site de divulgação científica dedicado ao campo da agricultura. Nosso objetivo é fornecer informações valiosas e atualizadas sobre práticas agrícolas, inovações tecnológicas, tendências de mercado e pesquisa científica relacionada à agricultura. Com uma equipe dedicada de alunos da UNIP apaixonados por ciência e agricultura, buscamos compartilhar conhecimentos e promover a disseminação de boas práticas para melhorar a produtividade, sustentabilidade e eficiência no setor agrícola. Explore nosso conteúdo e descubra as últimas descobertas e avanços na ciência agrícola."
+				property="og:description"
+			/>
+			<meta content="https://agro-site-design.vercel.app/metadata-image.png" property="og:image" />
+			<meta content="CultivaCiência - Sua melhor fonte de conhecimento" property="og:image:alt" />
+			<meta content="CultivaCiência" property="og:site_name" />
+
+			<meta content="summary_large_image" property="twitter:card" />
+			<meta content="https://agro-site-design.vercel.app/" property="twitter:url" />
+			<meta content="CultivaCiência - Sua melhor fonte de conhecimento" property="twitter:title" />
+			<meta
+				content="CultivaCiência é um site de divulgação científica dedicado ao campo da agricultura. Nosso objetivo é fornecer informações valiosas e atualizadas sobre práticas agrícolas, inovações tecnológicas, tendências de mercado e pesquisa científica relacionada à agricultura. Com uma equipe dedicada de alunos da UNIP apaixonados por ciência e agricultura, buscamos compartilhar conhecimentos e promover a disseminação de boas práticas para melhorar a produtividade, sustentabilidade e eficiência no setor agrícola. Explore nosso conteúdo e descubra as últimas descobertas e avanços na ciência agrícola."
+				property="twitter:description"
+			/>
+			<meta content="https://agro-site-design.vercel.app/metadata-image.png" property="twitter:image" />
+			<meta content="CultivaCiência - Sua melhor fonte de conhecimento" property="twitter:image:alt" />
+			<meta content="@cultivaciencia" property="twitter:site" />
+			<meta content="@cultivaciencia" property="twitter:creator" />
+
+			<meta content="Agricultura" property="article:section" />
+			<meta
+				content="agricultura, divulgação científica, UNIP, práticas agrícolas, inovações tecnológicas, tendências de mercado, pesquisa científica, produtividade, sustentabilidade, eficiência"
+				property="article:tag"
+			/>
+			<meta content="2023-05-21T00:35:24.093Z" property="article:published_time" />
+			<meta content="https://jpbm.dev/" property="article:author" />
+			<meta content="https://jpbm.dev/" property="article:publisher" />
+
+			<link href="https://agro-site-design.vercel.app/" rel="canonical" />
+			<link href="/favicon.ico" rel="icon" />
+		</head>
+	);
+}
