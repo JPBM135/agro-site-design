a1394ae331557888bf5a66870251385f568ad01b
João Pedro Borges Martins <jpedrobm0@gmail.com>
2023-05-20 22:35:13 -0300

    chore(/): add drop shadow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,9 @@ export default function Home() {
 			<div className="w-full">
 				<ImageCarousel />
 				<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-					<h1 className="text-6xl font-bold text-center text-white">CultivaCiência</h1>
+					<h1 className="text-6xl font-bold text-center text-white drop-shadow-[0_0_0.3rem_#ffffff70]">
+						CultivaCiência
+					</h1>
 					<h2 className="text-2xl font-semibold text-center text-white">Sua melhor fonte de conhecimento</h2>
 				</div>
 			</div>
