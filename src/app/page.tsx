462f725cad1b0d9aa60da1455a450fe5bd11c011
João Pedro Borges Martins <jpedrobm0@gmail.com>
2023-05-21 18:04:52 -0300

    chore(root): text-justify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import Image from 'next/image';
+import { useEffect } from 'react';
 import { ImageCarousel } from '@/components/ImageCarousel';
 
 export default function Home() {
 	return (
-		<main className="flex flex-col min-h-screen w-full gap-8 mt-10">
+		<main className="flex flex-col items-center min-h-screen w-full gap-8 mt-10">
 			<div className="w-full">
 				<ImageCarousel />
 				<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
@@ -17,7 +18,10 @@ export default function Home() {
 					</h2>
 				</div>
 			</div>
-			<div className="flex max-lg:flex-col gap-5 justify-evenly">
+			<h2 className="text-4xl max-lg:text-2xl font-semibold text-center border-b mb-2 pb-2 w-fit text-raisin-black">
+				Quais são os métodos de plantio mais utilizados?
+			</h2>
+			<div className="flex max-lg:flex-col gap-5 justify-evenly w-full">
 				<div className="max-lg:mx-5 max-w-sm flex flex-col items-center gap-5 bg-snow p-5 rounded-lg drop-shadow-xl">
 					<Image
 						alt="Imagem de uma plantação representando o plantio convencional"
@@ -29,7 +33,7 @@ export default function Home() {
 					<h1 className="text-2xl font-bold tracking-tighter text-center text-raisin-black drop-shadow-[0_0_0.3rem_#ffffff70]">
 						Plantio convencional
 					</h1>
-					<h4 className="text-center text-raisin-black">
+					<h4 className="text-justify text-raisin-black">
 						Este método usa técnicas tradicionais para o preparo do solo. Ele é mais utilizado em hortas domésticas e
 						lavouras comerciais, e exige a preparação do solo de forma mais intensiva.
 					</h4>
@@ -45,7 +49,7 @@ export default function Home() {
 					<h1 className="text-2xl font-bold tracking-tighter  text-center text-raisin-black drop-shadow-[0_0_0.3rem_#ffffff70]">
 						Plantio Direto
 					</h1>
-					<h4 className="text-center text-raisin-black">
+					<h4 className="text-justify text-raisin-black">
 						Neste caso, o plantio utiliza os restos da última colheita como matéria orgânica, feito por cima da palhada
 						seca da produção anterior, sendo essencial a rotação de cultura neste plantio.
 					</h4>
@@ -61,7 +65,7 @@ export default function Home() {
 					<h1 className="text-2xl font-bold tracking-tighter  text-center text-raisin-black drop-shadow-[0_0_0.3rem_#ffffff70]">
 						Plantio Minimo
 					</h1>
-					<h4 className="text-center text-raisin-black">
+					<h4 className="text-justify text-raisin-black">
 						Este sistema é a mistura do Direto e do Convencional, com o uso mínimo de máquinas agrícolas sobre o solo, e
 						exigindo poucas operações para a preparação do solo.
 					</h4>
